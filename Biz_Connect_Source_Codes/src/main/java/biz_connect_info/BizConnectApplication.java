0799e3744f899318ef435100e56a32b430a50dd1
Debashish <debasis.nath@gmail.com>
2026-05-24 00:23:19 +0530

    Auth Info is checked in
    Auth Info is checked in

--- Biz_Connect_Source_Codes/src/main/java/biz_connect_info/BizConnectApplication.java
+++ Biz_Connect_Source_Codes/src/main/java/biz_connect_info/BizConnectApplication.java
@@ -1,8 +1,14 @@
 package biz_connect_info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"auth_info", "biz_connect_info"}) // Scan both packages
+@EnableJpaRepositories(basePackages = {"auth_info.repository","biz_connect_info.repository"}) // Ensure repositories are detected
+@EntityScan(basePackages = {"auth_info.models","biz_connect_info.models"})
 public class BizConnectApplication {
 
 	public static void main(String[] args) {
